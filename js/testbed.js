6f229d5b5cbe179f858a6ace2bf522ea990bee85
Peter Gottesman <peter@petergottesman.com>
2018-10-21 02:36:23 -0400

    Arthur is better than me

--- js/testbed.js
+++ js/testbed.js
@@ -203,6 +203,7 @@ function addFurnishing(model, location, rotation, scene) {
             const mesh = meshes[0];
             mesh.setPositionWithLocalVector(location);
             mesh.rotation = rotation;
+	    localAxes(mesh);
             furnishings.push(mesh);
         });
 }
@@ -277,4 +278,36 @@ function findFurniture(search, facet_filters)
     return index.search(query).then(function(result){
 	return result;
     });
+}
+
+
+function localAxes(size, mesh) {
+    var pilot_local_axisX = BABYLON.Mesh.CreateLines("pilot_local_axisX", [
+	new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+	new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+    ], scene);
+    pilot_local_axisX.color = new BABYLON.Color3(1, 0, 0);
+
+    pilot_local_axisY = BABYLON.Mesh.CreateLines("pilot_local_axisY", [
+	new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+	new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+    ], scene);
+    pilot_local_axisY.color = new BABYLON.Color3(0, 1, 0);
+
+    var pilot_local_axisZ = BABYLON.Mesh.CreateLines("pilot_local_axisZ", [
+	new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0 , -0.05 * size, size * 0.95),
+	new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3( 0, 0.05 * size, size * 0.95)
+    ], scene);
+    pilot_local_axisZ.color = new BABYLON.Color3(0, 0, 1);
+
+    var local_origin = BABYLON.MeshBuilder.CreateBox("local_origin", {size:.1}, scene);
+    local_origin.isVisible = false;
+
+    pilot_local_axisX.parent = local_origin;
+    pilot_local_axisY.parent = local_origin;
+    pilot_local_axisZ.parent = local_origin;
+
+    local_origin.parent = mesh
+
+    return local_origin;
 }
